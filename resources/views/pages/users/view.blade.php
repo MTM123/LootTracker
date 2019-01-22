dc75c7c50b467a5e376d265fe7fcb3b995db7b9e
Hardijs Kivers <kulershk@gmail.com>
2019-01-22 22:06:42 +0200

    Monster kill scss and layout

--- resources/views/pages/users/view.blade.php
+++ resources/views/pages/users/view.blade.php
@@ -8,8 +8,41 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @foreach($user->sortedKills() as $kill)
-                        {{ $kill->name }} ({{ $kill->level }}) - {{ $kill->count }}<br>
+                    @php
+                        $lastLetter = "";
+                        $new = false;
+                        $list = $user->sortedKills();
+                    @endphp
+
+                    @foreach($list as $id => $kill)
+                        @php
+                            $currentLetter = substr($kill->name, 0, 1)
+                        @endphp
+
+                        @if($lastLetter != $currentLetter)
+                            @php
+                                $lastLetter = $currentLetter;
+                                $new = true;
+                            @endphp
+                        @endif
+
+                        @if($new == true)
+                            <div class="card col-6 user-monster-kill">
+                                <div class="card-body" >
+                                    <span class="text-center">{{ $lastLetter }}</span>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                        @endif
+                                    <li class="list-group-item">{{ $kill->name }} ({{ $kill->level }}) - {{ $kill->count }}</li>
+                        @if(@substr($list[$id+1]->name, 0, 1) != $currentLetter)
+                                </ul>
+                            </div>
+
+                        @endif
+
+                        @php
+                            $new = false;
+                        @endphp
                     @endforeach
                 </div>
             </div>
