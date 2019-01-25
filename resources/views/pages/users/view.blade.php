ec77d0cd4944c9e1625ab07ef665416654a49c10
Hardijs Kivers <hardijs.kivers@magebit.com>
2019-01-25 15:47:18 +0200

    Rename back text after auto deploy text

--- resources/views/pages/users/view.blade.php
+++ resources/views/pages/users/view.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-9">
             <div class="card">
-                <div class="card-header">Monsters!!</div>
+                <div class="card-header">Monsters</div>
 
                 <div class="card-body">
                         <div class="card-columns">
