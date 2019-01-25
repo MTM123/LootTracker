a3bd6f24ca29c28287b45ebdb5bfcfcc9d673165
Hardijs Kivers <hardijs.kivers@magebit.com>
2019-01-26 01:25:47 +0200

    MOntr drops

--- resources/views/pages/users/drops.blade.php
+++ resources/views/pages/users/drops.blade.php
@@ -3,7 +3,54 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        Here goes drop data for specific monsters
+
+        <div class="col-md-9">
+            <div class="card mb-3">
+                <div class="card-header">Graph</div>
+
+                <div class="card-body">
+
+                </div>
+            </div>
+
+            <div class="card mb-3">
+                <div class="card-header">Monster list</div>
+                    <?php
+                    $killKc = count($user->kills);
+                    $loot = [];
+
+                    foreach ($user->kills as $kills) {
+                        foreach ($kills->items as $items) {
+                            $loot[] = ['id' => $items->id, 'price' => $items->price, 'name' => $items->name, 'qty' => $items->pivot->item_qty];
+                        }
+                    }
+
+                    dd($loot);
+
+                    echo $killKc;
+                    //dd($user->kills->items)
+
+
+
+                //dd($user->kills[0]->items[0]->price);
+                ?>
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header">Drop List</div>
+
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+
+
     </div>
 </div>
 @endsection
